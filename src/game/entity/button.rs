597fb654e38c47297465b07ec5343f11b1e5e49e
Arthur Wuterich <arthurwut@gmail.com>
2022-12-15 05:04:17 -0800

    Add Button Click + Zombie Sound

--- src/game/entity/button.rs
+++ src/game/entity/button.rs
@@ -1,4 +1,5 @@
 use crate::app::App;
+use crate::core::component::audio_clip::AudioClip;
 use crate::core::component::component::Component;
 use crate::core::component::image::Image;
 use crate::core::entity::{Entity, EntityFns};
@@ -11,7 +12,11 @@ use sfml::window::Event as SFMLEvent;
 use core::any::Any;
 
 #[derive(Default, Debug, Clone, Copy)]
-struct Data {}
+struct Data {
+    x: f32,
+    y: f32,
+    button_down: bool,
+}
 
 impl Component for Data {
     fn get_name(&self) -> &str {
@@ -25,27 +30,62 @@ impl Component for Data {
     }
 }
 
-fn update_button(_e: &mut Entity, _app: &App, _dt: f32) {}
+fn update_button(e: &mut Entity, _app: &App, _dt: f32) {
+    let data = e.find_component::<Data>("data").unwrap().clone();
+    let over_button;
+    {
+        let render_offset_x = e.render_offset.x as f32;
+        let render_offset_y = e.render_offset.y as f32;
+        let button = e.find_component::<Image>("background").unwrap();
+        let bx = render_offset_x + button.x;
+        let by = render_offset_y + button.y;
+
+        let mx = data.x;
+        let my = data.y;
+        let half_width = button.width / 2.;
+        let half_height = button.height / 2.;
+        over_button = mx > bx - half_width
+            && mx < bx + half_width
+            && my > by - half_height
+            && my < by + half_height;
+
+        if over_button {
+            button.color.x = 0.;
+        } else {
+            button.color.x = 1.;
+        }
+    }
+
+    if data.button_down && over_button {
+        {
+            let beep = e.find_component::<AudioClip>("zombie").unwrap();
+            if beep.sound.get_sound().status() == sfml::audio::SoundStatus::STOPPED {
+                beep.sound.get_sound().play();
+            }
+        }
+    }
+}
+
 fn handle_event(e: &mut Entity, _app: &mut App, ev: &Event) {
-    let button = e.find_component::<Image>("background").unwrap();
+    let data = e.find_component::<Data>("data").unwrap();
     match ev {
         Event::SFMLEvent(sev) => match sev {
             SFMLEvent::MouseMoved { x, y } => {
-                let mx = *x as f32;
-                let my = *y as f32;
-                let half_width = button.width / 2.;
-                let half_height = button.height / 2.;
-
-                if mx > button.x - half_width
-                    && mx < button.x + half_width
-                    && my > button.y - half_height
-                    && my < button.y + half_height
-                {
-                    button.color.x = 0.;
-                } else {
-                    button.color.x = 1.;
-                }
+                data.x = *x as f32;
+                data.y = *y as f32;
             }
+            SFMLEvent::MouseButtonPressed { button, .. } => match button {
+                &sfml::window::mouse::Button::Left => {
+                    data.button_down = true;
+                }
+                _ => {}
+            },
+            SFMLEvent::MouseButtonReleased { button, .. } => match button {
+                &sfml::window::mouse::Button::Left => {
+                    data.button_down = false;
+                }
+                _ => {}
+            },
             _ => {}
         },
         _ => {}
@@ -70,6 +110,12 @@ pub fn make_button(app: &mut App, _viewport: &Viewport) -> Entity {
             .unwrap();
         let bg = Image::with_texture("background", bg_image.texture_id, 100., 100.);
         e.add_component(bg);
+    }
+
+    {
+        let audio_data = app.resource.load_audio_data("res/snd/zombie.wav").unwrap();
+        let zombie = AudioClip::new("zombie", &audio_data);
+        e.add_component(zombie);
     }
 
     return e;
